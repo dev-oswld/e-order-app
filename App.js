652b02d0212b1c5f8f2d859a49c1e9b2ddabc1e2
Oswld <user.oswld@gmail.com>
2019-11-09 23:44:00 -0600

    Working with API REST

--- App.js
+++ App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import AppNavigator from './AppNavigator';
+import axios from 'axios';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() { //restaurant/menu/complement/
+    axios.defaults.baseURL = 'https://dry-escarpment-18808.herokuapp.com';
+    axios.defaults.timeout = 81000;
+  }
+
   render() {
     return (
-      <AppNavigator/>
+      <AppNavigator />
     );
   }
 }
