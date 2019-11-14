fd7b00c4683dd3fa4b961efa260cb7de80f818d3
Oswld <user.oswld@gmail.com>
2019-11-13 21:44:12 -0600

    Bye bye smell code

--- tabs/MenuTabOne.js
+++ tabs/MenuTabOne.js
@@ -1,51 +1,27 @@
 import axios from 'axios';
 import React, { Component } from 'react';
-import { StyleSheet, FlatList, Text, View } from 'react-native';
-//import { Container, Content, CardItem, Body, Text, List } from 'native-base';
+import { StyleSheet } from 'react-native';
+import { Container, Content, CardItem, Body, Text, List } from 'native-base';
 
 export default class MenuTabOne extends Component {
-    /*constructor(props) {
+    constructor(props) {
         super(props);
-        const spaceKey = {
-            "spaceKey": 'S_70f6cb48-9609-4641-b2e7-af242d4968b0'
-        }
-       
-    }*/
+    }
 
     state = {
         data: []
     };
 
     componentDidMount() {
-        this.fetchData();
-    }
 
-    fetchData = async () => { //Bad example
-        const response = await fetch("https://randomuser.me/api?results=10");
-        const json = await response.json();
-        this.setState({ data: json.results });
-    };
+    }
 
     render() {
         return (
-
-
-            <View>
-
-
-                <FlatList
-                    data={this.state.data}
-                    keyExtractor={(x, i) => i}
-                    renderItem={({ item }) =>
-                        <Text>
-                            {`${item.name.first} ${item.name.last}`}
-                        </Text>}
-                />
-            </View>
-            /*<Container style={styles.container}>
-           <Content style={styles.content}>   
-           </Content>
-       </Container>*/
+            <Container style={styles.container}>
+                <Content style={styles.content}>
+                </Content>
+            </Container>
         );
     }
 }
