097de0b6ae4eda6c6028128fd7af0b4be869e570
Oswld <user.oswld@gmail.com>
2019-11-18 19:04:34 -0600

    Axios post

--- screens/Register.js
+++ screens/Register.js
@@ -1,19 +1,77 @@
+import axios from 'axios';
 import React, { Component } from 'react';
 import { StyleSheet, ToastAndroid } from 'react-native';
 import { Container, Header, Title, Button, Icon, Left, Right, Body, Content, Form, Item, Input, Text, Thumbnail } from 'native-base';
 
 export default class Register extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      visible: false
-    };
+  state = {
+    username: '',
+    first_name: '',
+    last_name: '',
+    email: '',
+    password: '',
+    //hiddenPassword: true,
   }
 
-  showToast = () => {
-    ToastAndroid.show("Registro exitoso", ToastAndroid.SHORT);
+  /* clearInputs() {
+    this.setState({ username: '' });
+    this.setState({ first_name: '' });
+    this.setState({ last_name: '' });
+    this.setState({ email: '' });
+    this.setState({ password: '' });
+  } */
+
+  createUser() {
+    const payload = { "username": this.state.username, "password": this.state.password, "email": this.state.email, "first_name": this.state.first_name, "last_name": this.state.last_name }
+    console.log('Payload data: ', payload);
+
+    if (payload.username == '') {
+      ToastAndroid.show('Se debe ingresar un usuario', ToastAndroid.SHORT);
+    } else if (payload.first_name == '') {
+      ToastAndroid.show('Se debe ingresar el primer nombre propio', ToastAndroid.SHORT);
+    } else if (payload.last_name == '') {
+      ToastAndroid.show('Se debe ingresar el segundo nombre propio', ToastAndroid.SHORT);
+    } else if (payload.email == '') {
+      ToastAndroid.show('Se debe ingresar un email', ToastAndroid.SHORT);
+    } else if (payload.password == '') {
+      ToastAndroid.show('Se debe ingresar una contraseña', ToastAndroid.SHORT);
+    } else {
+      axios.post('/auth/register/', payload)
+        .then(response => {
+          //console.log(response.data);
+          token = response.data.token;
+          axios.defaults.headers.common.Authorization = `Token ${token}`;
+          console.log('Tu token es: ', token);
+          ToastAndroid.show("Registro exitoso", ToastAndroid.SHORT);
+        })
+        .catch(error => {
+          console.log(error.response);
+          ToastAndroid.show('Error al crear la cuenta nueva', ToastAndroid.SHORT);
+        });
+    }
   }
 
+  onUsernameChange(text) {
+    this.setState({ username: text });
+  }
+
+  onFirstNameChange(text) {
+    this.setState({ first_name: text });
+  }
+
+  onLastNameChange(text) {
+    this.setState({ last_name: text });
+  }
+
+  onEmailChange(text) {
+    this.setState({ email: text });
+  }
+
+  onPasswordChange(text) {
+    this.setState({ password: text });
+  }
+
+  // setVisibility = () => { this.setState({ hidePassword: !this.state.hidePassword }); } Fix bugs!
   render() {
     console.log("* Register : NEW *");
     return (
@@ -33,22 +91,48 @@ export default class Register extends Component {
           <Thumbnail style={styles.image} source={require("../assets/images/map.png")} />
           <Form>
             <Item style={styles.item} rounded stackedLabel last>
-              <Input style={styles.input} placeholder='Ingrese el nombre de la sucursal' placeholderTextColor="white" />
+              <Input
+                style={styles.input}
+                placeholder='Ingrese el nombre de usuario'
+                placeholderTextColor="white"
+                onChangeText={this.onUsernameChange.bind(this)}
+              />
             </Item>
             <Item style={styles.item} rounded stackedLabel last>
-              <Input style={styles.input} placeholder='Ingrese el primer nombre propio' placeholderTextColor="white" />
+              <Input
+                style={styles.input}
+                placeholder='Ingrese el primer nombre propio'
+                placeholderTextColor="white"
+                onChangeText={this.onFirstNameChange.bind(this)}
+              />
             </Item>
             <Item style={styles.item} rounded stackedLabel last>
-              <Input style={styles.input} placeholder='Ingrese el segundo nombre propio' placeholderTextColor="white" />
+              <Input
+                style={styles.input}
+                placeholder='Ingrese el segundo nombre propio'
+                placeholderTextColor="white"
+                onChangeText={this.onLastNameChange.bind(this)}
+              />
             </Item>
             <Item style={styles.item} rounded stackedLabel last>
-              <Input style={styles.input} placeholder='Ingrese un email' placeholderTextColor="white" />
+              <Input
+                style={styles.input}
+                placeholder='Ingrese un email'
+                placeholderTextColor="white"
+                onChangeText={this.onEmailChange.bind(this)}
+              />
             </Item>
             <Item style={styles.item} rounded stackedLabel last>
-              <Input style={styles.input} secureTextEntry={true} placeholder='Ingrese la contraseña' placeholderTextColor="white" />
+              <Input
+                style={styles.input}
+                secureTextEntry={false} //setVisibility?
+                placeholder='Ingrese la contraseña'
+                placeholderTextColor="white"
+                onChangeText={this.onPasswordChange.bind(this)}
+              />
             </Item>
           </Form>
-          <Button block rounded large style={styles.button} onPress={this.showToast}>
+          <Button block rounded large style={styles.button} onPress={this.createUser.bind(this)}>
             <Text>Registrar</Text>
           </Button>
         </Content>
